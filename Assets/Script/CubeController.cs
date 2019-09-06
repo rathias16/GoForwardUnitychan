1fc78082644b0cfb56431dd016ac62a002e58bdb
yumi <cblog1201@gmail.com>
2019-09-06 19:37:59 +0900

    Lesson7課題提出用です。

---
+++ Assets/Script/CubeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+    private float speed = -0.2f;
+
+    private float deadline = -10;
+
+    AudioSource audiosource;
+
+    public AudioClip block;
+	// Use this for initialization
+	void Start () {
+        audiosource = GetComponent<AudioSource>();
+
+        audiosource.clip = block;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(this.speed, 0, 0);
+
+        if(transform.position.x < this.deadline)
+        {
+            Destroy(gameObject);
+        }
+	}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "GroundTag" || collision.gameObject.tag == "BlockTag")
+        {
+            audiosource.Play();
+        }
+    }
+}
